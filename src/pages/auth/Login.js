82626559cb13236e6624231619cabd67126060d2
Ajay Mehra <ajaymehra4492@gmail.com>
2023-12-24 15:45:10 +0530

    Updating login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -19,6 +19,7 @@ const location=useLocation();
 const handleSubmit=async(e)=>{
     e.preventDefault();
     try{
+      console.log(email,password);
     const res= await axios.post(`https://ecommerce-api-bay-three.vercel.app/api/v1/auth/login`,
     {email,password});
     if(res.data.success){
